66929198f9dc5c669e4e52cdcb8745a6f8c27fc1
shravani <shravani26@gmail.com>
2025-08-29 09:05:00 +0530

    off-heap cache

---
+++ src/main/java/com/example/javalocaloffheapcache/JavaLocalOffHeapCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.javalocaloffheapcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLocalOffHeapCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLocalOffHeapCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javalocaloffheapcache/JavaLocalOffHeapCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javalocaloffheapcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLocalOffHeapCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
